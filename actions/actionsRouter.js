697779e4db1a2ef9344869456c2ddf39242d05e3
Neha Patel <neha.r.p@gmail.com>
2019-09-06 09:45:13 -0700

    wrote out in notes thought process

---
+++ actions/actionsRouter.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router
+
+//get array of all actions per project(project_id)
+
+
+//insert action (post) (project_id, description (128 character max), and notes)
+
+//update action (description and notes)
+
+//remove action
+
+//toggle completed (not required, boolean)
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const router = express.Router
 
+//get array of all projects
 
+//get project with id
+
+//insert project (post) (name and description)
+
+//update project (name and description)
+
+//remove project
+
+//toggle completed (not required, boolean)
 
 
 
